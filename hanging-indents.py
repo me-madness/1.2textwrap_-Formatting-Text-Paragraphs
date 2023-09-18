271001d9fbbae8d6ee50b9cf71f456ea1ac56d67
me-madness <58656549+me-madness@users.noreply.github.com>
2023-09-18 20:47:54 +0200

    1.2.6 Finish

--- hanging-indents.py
+++ hanging-indents.py
@@ -1,2 +1,9 @@
 import textwrap
 from example_data import sample_text
+
+dedented_text = textwrap.dedent(sample_text).strip()
+print(textwrap.fill(dedented_text,
+                    initial_indent='', \
+                    subsequent_indent=' ' * 4,
+                    width=50,
+                    ))
